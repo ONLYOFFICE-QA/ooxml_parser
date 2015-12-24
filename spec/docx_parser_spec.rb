25cef9850b26082740f32773cb2382c3623ac228
Pavel Lobashov <shockwavenn@gmail.com>
2015-12-24 12:16:21 +0300

    Remove russian messages from pending

--- spec/docx_parser_spec.rb
+++ spec/docx_parser_spec.rb
@@ -832,14 +832,14 @@ describe 'My behaviour' do
         end
 
         it 'copied_paragrph_with_color_1' do
-          pending('Проблема с цветам. Лузянин не смог объяснить логику')
+          pending('Color problems. I cannot solve it')
           docx = OoxmlParser::DocxParser.parse_docx('spec/docx_examples/elements/paragraph/runs/font_color/copied_paragrph_with_color_1.docx')
           expect(docx.elements.first.character_style_array.first.font_color).to eq(OoxmlParser::Color.new(197, 216, 240))
           expect(docx.elements.last.character_style_array.first.font_color).to eq(OoxmlParser::Color.new(146, 208, 80))
         end
 
         it 'copied_paragrph_with_color_2' do
-          pending('Проблема с цветам. Лузянин не смог объяснить логику')
+          pending('Color problems. I cannot solve it')
           docx = OoxmlParser::DocxParser.parse_docx('spec/docx_examples/elements/paragraph/runs/font_color/copied_paragrph_with_color_2.docx')
           expect(docx.elements.first.character_style_array.first.font_color).to eq(OoxmlParser::Color.new(191, 191, 191))
           expect(docx.elements.last.character_style_array.first.font_color).to eq(OoxmlParser::Color.new(95, 74, 121))
