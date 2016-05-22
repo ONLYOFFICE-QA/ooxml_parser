33a15d5b66f86c6cb1f473c5a23ca8fd79c3bebc
Pavel Lobashov <shockwavenn@gmail.com>
2016-05-22 13:18:19 +0300

    Fix parsing nil borders

--- spec/docx_parser_spec.rb
+++ spec/docx_parser_spec.rb
@@ -734,6 +734,14 @@ describe 'My behaviour' do
           docx = OoxmlParser::DocxParser.parse_docx('spec/docx_examples/elements/table/row/table_row_height.docx')
           expect(docx.elements[1].rows[1].table_row_properties.height).to eq(1000)
         end
+
+        it 'border_color_nil' do
+          docx = OoxmlParser::DocxParser.parse_docx('spec/docx_examples/elements/table/row/border_color_nil.docx')
+          expect(docx.elements.first.properties.table_borders.left).to be_nil
+          expect(docx.elements.first.properties.table_borders.right).to be_nil
+          expect(docx.elements.first.properties.table_borders.top).to be_nil
+          expect(docx.elements.first.properties.table_borders.bottom).to be_nil
+        end
       end
     end
   end
