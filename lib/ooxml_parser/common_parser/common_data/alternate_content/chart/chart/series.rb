670ef24ca7e5b576113a57f946624296b278bd16
Pavel Lobashov <shockwavenn@gmail.com>
2016-07-08 12:42:30 +0300

    Add parsing of series order value

--- lib/ooxml_parser/common_parser/common_data/alternate_content/chart/chart/series.rb
+++ lib/ooxml_parser/common_parser/common_data/alternate_content/chart/chart/series.rb
@@ -1,9 +1,12 @@
+require_relative 'series/order'
 require_relative 'series/series_index'
 module OoxmlParser
   # Class for parsing `c:ser` object
   class Series < OOXMLDocumentObject
     # @return [Index] index of chart
     attr_accessor :index
+    # @return [Order] order of chart
+    attr_accessor :order
 
     # Parse Series
     # @param [Nokogiri::XML:Node] node with Series
@@ -14,6 +17,8 @@ module OoxmlParser
         case series_childe_node.name
         when 'idx'
           series.index = SeriesIndex.parse(series_childe_node)
+        when 'order'
+          series.order = Order.parse(series_childe_node)
         end
       end
       series
