3c44ae8d78824cf192c4aa22e98c693b7400d911
Pavel Lobashov <shockwavenn@gmail.com>
2016-07-27 17:46:57 +0300

    Separate parameters for font size and complex font size

--- spec/document/elements/paragraph/runs/runs_font_size_spec.rb
+++ spec/document/elements/paragraph/runs/runs_font_size_spec.rb
@@ -5,4 +5,9 @@ describe 'Runs Font Size' do
     docx = OoxmlParser::DocxParser.parse_docx('spec/document/elements/paragraph/runs/size/font_size_float.docx')
     expect(docx.elements[0].character_style_array[1].size).to eq(53.5)
   end
+
+  it 'default_font_size' do
+    docx = OoxmlParser::DocxParser.parse_docx('spec/document/elements/paragraph/runs/size/default_font_size.docx')
+    expect(docx.elements.first.character_style_array[1].size).to eq(15)
+  end
 end
