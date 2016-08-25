939a19f212785c87049694c32904d646c18a08e6
Pavel Lobashov <shockwavenn@gmail.com>
2016-08-25 12:32:13 +0300

    Move methods to helper and fix for OoxmlSize

--- lib/ooxml_parser/pptx_parser/pptx_data/presentation/slide.rb
+++ lib/ooxml_parser/pptx_parser/pptx_data/presentation/slide.rb
@@ -3,61 +3,15 @@ require_relative 'slide/background'
 require_relative 'slide/graphic_frame/graphic_frame'
 require_relative 'slide/slide/shapes_grouping'
 require_relative 'slide/slide/timing'
+require_relative 'slide/slide_helper'
 module OoxmlParser
   class Slide < OOXMLDocumentObject
+    include SlideHelper
     attr_accessor :elements, :background, :transition, :timing, :alternate_content
 
     def initialize(elements = [], background = nil)
       @elements = elements
       @background = background
-    end
-
-    def nonempty_elements
-      @elements.select { |cur_shape| !cur_shape.text_body.paragraphs.first.characters.empty? }
-    end
-
-    def graphic_frames
-      @elements.select { |cur_element| cur_element.is_a?(GraphicFrame) }
-    end
-
-    # Get transform property of object, by object type
-    # @param object [Symbol] type of object: :image, :chart, :table, :shape
-    # @return [OOXMLDocumentObject] needed object
-    def transform_of_object(object)
-      case object
-      when :image
-        return elements.find { |e| e.is_a? Picture }.properties.transform
-      when :chart
-        return elements.find { |e| e.is_a? GraphicFrame }.transform
-      when :table
-        return elements.find { |e| e.is_a? GraphicFrame }.transform
-      when :shape
-        return elements.find { |e| !e.shape_properties.preset.nil? }.shape_properties.transform
-      else
-        raise "Dont know this type object - #{object}"
-      end
-    end
-
-    def content_horizontal_align(object, slide_size)
-      transform = transform_of_object(object)
-      return :left if transform.offset.x.zero?
-      return :center if ((slide_size.width / 2) - (transform.extents.x / 2)).round(1) == transform.offset.x.round(1)
-      return :right if (slide_size.width - transform.extents.x).round(1) == transform.offset.x.round(1)
-      :unknown
-    end
-
-    def content_vertical_align(object, slide_size)
-      transform = transform_of_object(object)
-      return :top if transform.offset.y.zero?
-      return :middle if ((slide_size.height / 2) - (transform.extents.y / 2)).round(1) == transform.offset.y.round(1)
-      return :bottom if (slide_size.height - transform.extents.y).round(1) == transform.offset.y.round(1)
-      :unknown
-    end
-
-    def content_distribute(object, slide_size)
-      return [:horizontally, :vertically] if content_horizontal_align(object, slide_size) == :center && content_vertical_align(object, slide_size) == :middle
-      return [:horizontally] if content_horizontal_align(object, slide_size) == :center
-      return [:vertically] if content_vertical_align(object, slide_size) == :middle
     end
 
     def self.parse(path_to_slide_xml)
