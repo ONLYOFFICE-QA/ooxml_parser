af2fbf832e706316b653708a5fe0a14bbc645498
Pavel Lobashov <shockwavenn@gmail.com>
2016-09-01 18:27:13 +0300

    Correct parsing `Margins` for `tcPr`

--- spec/document/elements/table/cell/properties/table_cell_margins_spec.rb
+++ spec/document/elements/table/cell/properties/table_cell_margins_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'TableCellMargins' do
   it 'table_cell_margins_size' do
-    docx = OoxmlParser::DocxParser.parse_docx('spec/document/elements/table/cell/properties/margins/table_cell_margins_size.docx')
+    docx = OoxmlParser::DocxParser.parse_docx('spec/document/elements/table/cell/properties/table_cell_margin/table_cell_margins_size.docx')
     expect(docx.elements[1].properties.table_style.table_properties.table_cell_margin.top).to eq(OoxmlParser::OoxmlSize.new(1.27, :centimeter))
   end
 end
