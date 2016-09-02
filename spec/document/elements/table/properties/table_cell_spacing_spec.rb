992599ac322c581ea30ef7a002c804fec6b81727
Pavel Lobashov <shockwavenn@gmail.com>
2016-09-02 16:21:28 +0300

    Add table cell spacing for TableProperties

---
+++ spec/document/elements/table/properties/table_cell_spacing_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe 'My behaviour' do
+  it 'table_cell_spacing' do
+    docx = OoxmlParser::Parser.parse('spec/document/elements/table/properties/table_cell_spacing/table_cell_spacing.docx')
+    expect(docx.elements[1].properties.table_cell_spacing).to eq(OoxmlParser::OoxmlSize.new(1.27, :centimeter))
+  end
+end
