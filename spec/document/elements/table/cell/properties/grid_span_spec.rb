bd6b279b8bc3e8c46451991a8c8a260ebd95b39a
Pavel Lobashov <shockwavenn@gmail.com>
2016-09-02 16:59:13 +0300

    Remove merge alias, because it was confusing
    In debug it show another value, that in actual code
    See #172

--- spec/document/elements/table/cell/properties/grid_span_spec.rb
+++ spec/document/elements/table/cell/properties/grid_span_spec.rb
@@ -5,4 +5,9 @@ describe 'GridSpan' do
     docx = OoxmlParser::DocxParser.parse_docx('spec/document/elements/table/cell/properties/grid_span/grid_span_value.docx')
     expect(docx.elements[1].rows[1].cells[1].properties.grid_span.value).to eq(2)
   end
+
+  it 'remove_merge_alias' do
+    docx = OoxmlParser::DocxParser.parse_docx('spec/document/elements/table/cell/properties/grid_span/remove_merge_alias.docx')
+    expect(docx.elements[1].rows[1].cells[1].properties.methods).not_to include(:merge)
+  end
 end
