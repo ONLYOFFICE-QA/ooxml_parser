c8b4c8613d8e0ee0e716f1d9be9742063bda25e0
Pavel Lobashov <shockwavenn@gmail.com>
2016-09-15 13:38:49 +0300

    `RunSpacing#value` is in OoxmlSize

---
+++ spec/document/elements/paragraph/runs/properties/spacing_run_pr_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe 'Spacing' do
+  it 'spacing_sizing' do
+    docx = OoxmlParser::DocxParser.parse_docx('spec/document/elements/paragraph/runs/properties/spacing/spacing_sizing.docx')
+    expect(docx.elements.first.nonempty_runs[1].run_properties.spacing.value).to eq(OoxmlParser::OoxmlSize.new(80, :twip))
+  end
+end
