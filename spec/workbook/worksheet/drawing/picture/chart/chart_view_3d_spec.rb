6fbe35a85dbc8d54b841150cd86ae9998ffe72bd
Pavel Lobashov <shockwavenn@gmail.com>
2017-03-30 10:45:19 +0300

    Change interface of `XlsxDrawing#graphic_frame`

--- spec/workbook/worksheet/drawing/picture/chart/chart_view_3d_spec.rb
+++ spec/workbook/worksheet/drawing/picture/chart/chart_view_3d_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe 'My behaviour' do
   it 'chart_series_not_nil' do
     xlsx = OoxmlParser::XlsxParser.parse_xlsx('spec/workbook/worksheet/drawing/picture/chart/chart_3d_view/rotation_x.xlsx')
-    expect(xlsx.worksheets[0].drawings[0].picture.chart.view_3d.rotation_x.value).to eq(90)
+    expect(xlsx.worksheets[0].drawings[0].graphic_frame.graphic_data.first.view_3d.rotation_x.value).to eq(90)
   end
 end
