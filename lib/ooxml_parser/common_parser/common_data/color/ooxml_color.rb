f053fc71867234432a1d6882bd041d86f794ca02
Pavel Lobashov <shockwavenn@gmail.com>
2017-08-02 16:28:07 +0300

    Ooxml_color support there and tint

--- lib/ooxml_parser/common_parser/common_data/color/ooxml_color.rb
+++ lib/ooxml_parser/common_parser/common_data/color/ooxml_color.rb
@@ -7,6 +7,20 @@ module OoxmlParser
     attr_reader :theme_color
     # @return [Symbol] theme shade
     attr_reader :theme_shade
+    # @return [Integer] theme index
+    attr_reader :theme
+    # @return [Float] tint
+    attr_reader :tint
+
+    def to_color
+      return ThemeColors.parse_color_theme(theme, tint) if theme
+      value
+    end
+
+    def ==(other)
+      return to_color == other if other.is_a?(Color)
+      super
+    end
 
     # Parse Hyperlink object
     # @param node [Nokogiri::XML:Element] node to parse
@@ -20,8 +34,13 @@ module OoxmlParser
           @theme_color = value.value.to_sym
         when 'themeShade'
           @theme_shade = Integer("0x#{value.value}")
+        when 'theme'
+          @theme = value.value.to_i
+        when 'tint'
+          @tint = value.value.to_f
         end
       end
+      self
     end
   end
 end
