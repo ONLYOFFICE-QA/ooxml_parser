21ea450efefcd3b07aef642f66d16f352b8e9b63
Pavel Lobashov <shockwavenn@gmail.com>
2017-11-20 11:32:22 +0300

    Redone parsing `DocumentBackground#fill`

--- lib/ooxml_parser/xlsx_parser/xlsx_data/view_model/workbook/style_sheet/fills/fill.rb
+++ lib/ooxml_parser/xlsx_parser/xlsx_data/view_model/workbook/style_sheet/fills/fill.rb
@@ -4,11 +4,31 @@ module OoxmlParser
   class Fill < OOXMLDocumentObject
     # @return [PatternFill] pattern fill
     attr_accessor :pattern_fill
+    # @return [Color] second color
+    attr_reader :color2
+    # @return [String] id of file
+    attr_reader :id
+    # @return [FileReference] file of fill
+    attr_reader :file
+    # @return [Symbol] value
+    attr_reader :value
 
     # Parse Fill data
     # @param [Nokogiri::XML:Element] node with Fill data
     # @return [Fill] value of Fill data
     def parse(node)
+      node.attributes.each do |key, value|
+        case key
+        when 'color2'
+          @color2 = Color.new(parent: self).parse_hex_string(value.value.split(' ').first.delete('#'))
+        when 'id'
+          @id = value.value.to_s
+          @file = FileReference.new(parent: self).parse(node)
+        when 'type'
+          @type = value_to_symbol(value)
+        end
+      end
+
       node.xpath('*').each do |node_child|
         case node_child.name
         when 'patternFill'
