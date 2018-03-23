862621eb1f861f24fd284ac832793c29acc81959
Pavel Lobashov <shockwavenn@gmail.com>
2018-03-23 15:42:03 +0300

    Remove redundant `PointCount`

--- lib/ooxml_parser/common_parser/common_data/alternate_content/chart/chart/series/series_text/string_reference/string_cache.rb
+++ lib/ooxml_parser/common_parser/common_data/alternate_content/chart/chart/series/series_text/string_reference/string_cache.rb
@@ -1,10 +1,9 @@
-require_relative 'string_cache/point_count'
 require_relative 'string_cache/point'
 module OoxmlParser
   # Class for parsing `c:tx` object
   class StringCache < OOXMLDocumentObject
     # @return [StringReference] String reference of series
-    attr_accessor :point_count
+    attr_reader :point_count
     # @return [Array, Point] array of points
     attr_accessor :points
 
@@ -20,7 +19,7 @@ module OoxmlParser
       node.xpath('*').each do |node_child|
         case node_child.name
         when 'ptCount'
-          @point_count = PointCount.new(parent: self).parse(node_child)
+          @point_count = ValuedChild.new(:integer, parent: self).parse(node_child)
         when 'pt'
           @points << Point.new(parent: self).parse(node_child)
         end
