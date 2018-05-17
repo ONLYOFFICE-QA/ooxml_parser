4abafa015d9402378a4f189bad463fa8870f27ae
Pavel Lobashov <ShockwaveNN@gmail.com>
2018-05-17 14:14:34 +0300

    Fix crash on docx with no `theme.xml` (#491)

--- spec/document/theme_color_spec.rb
+++ spec/document/theme_color_spec.rb
@@ -5,4 +5,9 @@ describe 'Theme Color' do
     docx = OoxmlParser::Parser.parse('spec/document/theme_color/000_lastClr.docx')
     expect(docx.theme_colors.color_scheme[:dk1].color).to eq(OoxmlParser::Color.new(0, 0, 0))
   end
+
+  it 'Do not crash if there is no theme.xml file' do
+    docx = OoxmlParser::Parser.parse('spec/document/theme_color/no_theme_xml.docx')
+    expect(docx.theme_colors).to be_nil
+  end
 end
