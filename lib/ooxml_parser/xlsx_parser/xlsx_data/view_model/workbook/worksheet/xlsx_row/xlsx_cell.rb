8f5b19032492ee357c235433beff9f7538cfd7fb
Pavel Lobashov <shockwavenn@gmail.com>
2018-05-22 15:14:41 +0300

    Remove one nil check

--- lib/ooxml_parser/xlsx_parser/xlsx_data/view_model/workbook/worksheet/xlsx_row/xlsx_cell.rb
+++ lib/ooxml_parser/xlsx_parser/xlsx_data/view_model/workbook/worksheet/xlsx_row/xlsx_cell.rb
@@ -21,10 +21,10 @@ module OoxmlParser
       text_string_id = nil
       text_string_id = node.attribute('s').value if node.attribute('s')
       @style = CellStyle.new(parent: self).parse(text_string_id)
-      if node.attribute('t').nil?
-        @raw_text = node.xpath('xmlns:v').text
-      else
+      if node.attribute('t')
         node.attribute('t').value == 's' ? get_shared_string(node.xpath('xmlns:v').text) : @raw_text = node.xpath('xmlns:v').text
+      else
+        @raw_text = node.xpath('xmlns:v').text
       end
       @formula = node.xpath('xmlns:f').text unless node.xpath('xmlns:f').text == ''
       @text = @raw_text.dup unless @raw_text.nil?
