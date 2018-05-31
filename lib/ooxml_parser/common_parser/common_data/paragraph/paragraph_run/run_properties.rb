79569585b2e4bf8e70731f453676f515910bb126
Pavel Lobashov <shockwavenn@gmail.com>
2018-05-31 18:55:13 +0300

    Fix nil check

--- lib/ooxml_parser/common_parser/common_data/paragraph/paragraph_run/run_properties.rb
+++ lib/ooxml_parser/common_parser/common_data/paragraph/paragraph_run/run_properties.rb
@@ -123,7 +123,7 @@ module OoxmlParser
       end
       @font_color = DocxColorScheme.new(parent: self).parse(node)
       @font_name = root_object.default_font_typeface if @font_name.empty?
-      @font_size = root_object.default_font_size if @font_size.nil?
+      @font_size = root_object.default_font_size unless @font_size.nil?
       self
     end
   end
