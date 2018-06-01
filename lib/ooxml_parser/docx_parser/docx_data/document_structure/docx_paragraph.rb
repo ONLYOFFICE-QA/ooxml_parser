24fcdc99f75c6c2b66b1d92bdb3d7462e63959aa
Pavel Lobashov <shockwavenn@gmail.com>
2018-06-01 14:39:38 +0300

    Remove nil check

--- lib/ooxml_parser/docx_parser/docx_data/document_structure/docx_paragraph.rb
+++ lib/ooxml_parser/docx_parser/docx_data/document_structure/docx_paragraph.rb
@@ -136,7 +136,7 @@ module OoxmlParser
           character_style.parse(node_child, char_number, parent: self)
           character_style.comments = comments.dup
           character_styles_array << character_style.dup
-          character_styles_array.last.shape = character_style.shape unless character_style.shape.nil?
+          character_styles_array.last.shape = character_style.shape if character_style.shape
           char_number += 1
         when 'hyperlink'
           @hyperlink = Hyperlink.new(parent: self).parse(node_child)
