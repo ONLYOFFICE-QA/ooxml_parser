fd492fd9bdfdb3be250db24d7b55f80b82aa399e
Pavel Lobashov <shockwavenn@gmail.com>
2018-08-09 18:31:26 +0300

    Fix more cases of comments

---
+++ lib/ooxml_parser/pptx_parser/pptx_data/presentation/presentation_comments/presentation_comment.rb
@@ -0,0 +1,37 @@
+module OoxmlParser
+  # Class for parsing `p:cm` tag
+  class PresentationComment < OOXMLDocumentObject
+    # @return [Integer] id of author
+    attr_reader :author_id
+    # @return [OOXMLCoordinates] position of comment
+    attr_reader :position
+    # @return [String] text of comment
+    attr_reader :text
+
+    # @return [CommentAuthor] author of comment
+    def author
+      root_object.comment_authors.author_by_id(@author_id)
+    end
+
+    # Parse PresentationComment object
+    # @param node [Nokogiri::XML:Element] node to parse
+    # @return [PresentationComment] result of parsing
+    def parse(node)
+      node.attributes.each do |key, value|
+        case key
+        when 'authorId'
+          @author_id = value.value.to_i
+        end
+      end
+      node.xpath('*').each do |node_child|
+        case node_child.name
+        when 'pos'
+          @position = OOXMLCoordinates.parse(node_child, x_attr: 'x', y_attr: 'y')
+        when 'text'
+          @text = node_child.text.to_s
+        end
+      end
+      self
+    end
+  end
+end
--- spec/presentation/comments_spec.rb
+++ spec/presentation/comments_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'My behaviour' do
   it 'PresentationComment' do
     pptx = OoxmlParser::PptxParser.parse_pptx('spec/presentation/comments/comments.pptx')
-    expect(pptx.comments.first.text).to eq('Is it true?')
-    expect(pptx.comments.first.author.name).to eq('Hamish Mitchell')
+    expect(pptx.comments.list.first.text).to eq('Is it true?')
+    expect(pptx.comments.list.first.author.name).to eq('Hamish Mitchell')
   end
 end
