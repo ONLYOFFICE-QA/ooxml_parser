4467fda91f4e309e1e794e88614f87eff877460f
Pavel Lobashov <ShockwaveNN@gmail.com>
2019-03-06 11:26:58 +0300

    Store all_formula_values it single spec (#567)

--- spec/workbook/all_formula_values_spec.rb
+++ spec/workbook/all_formula_values_spec.rb
@@ -5,4 +5,9 @@ describe 'Workbook#all_formula_values' do
     xlsx = OoxmlParser::XlsxParser.parse_xlsx('spec/workbook/all_formula_values_spec/value_of_empty_f.xlsx')
     expect(xlsx.all_formula_values).to eq(['6.0', 'Error in calculation', '0.0', '6.0', '6.0', 'Error in calculation', '0.0'])
   end
+
+  it 'all_formulas_values' do
+    xlsx = OoxmlParser::XlsxParser.parse_xlsx('spec/workbook/all_formula_values_spec/all_formulas_values.xlsx')
+    expect(xlsx.all_formula_values.length).to eq(12)
+  end
 end
