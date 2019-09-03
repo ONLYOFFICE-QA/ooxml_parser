1a5cdccc01a468b48479690a6e89e8b6b52cbfd1
Pavel Lobashov <shockwavenn@gmail.com>
2019-09-03 10:54:34 +0300

    Add parsing `ExcelComment#text`

--- lib/ooxml_parser/xlsx_parser/xlsx_data/view_model/workbook/worksheet/excel_comments/excel_comment.rb
+++ lib/ooxml_parser/xlsx_parser/xlsx_data/view_model/workbook/worksheet/excel_comments/excel_comment.rb
@@ -2,6 +2,8 @@ module OoxmlParser
   # Single Comment of XLSX
   class ExcelComment < OOXMLDocumentObject
     attr_accessor :characters
+    # @return [String] text of comment
+    attr_reader :text
 
     def initialize(parent: nil)
       @characters = []
@@ -14,6 +16,12 @@ module OoxmlParser
     def parse(node)
       node.xpath('xmlns:text/xmlns:r').each do |node_child|
         @characters << ParagraphRun.new(parent: self).parse(node_child)
+      end
+      node.xpath('xmlns:text/*').each do |node_child|
+        case node_child.name
+        when 't'
+          @text = Text.new(parent: self).parse(node_child)
+        end
       end
       self
     end
