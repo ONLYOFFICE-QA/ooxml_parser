889badf10e8a93d86396a49ac63a3fb2c4a40c84
Pavel Lobashov <ShockwaveNN@gmail.com>
2019-10-24 14:29:58 +0300

    Add `Shade#to_s` (#581)

---
+++ spec/common/classes/shade_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe OoxmlParser::Shade do
+  let(:shade) do
+    OoxmlParser::Shade.new(value: :clear,
+                           color: :auto,
+                           fill: OoxmlParser::Color.new(1, 2, 3))
+  end
+
+  it 'OoxmlParser::Shade#to_s include fill color' do
+    expect(shade.to_s).to include('RGB (')
+  end
+end
