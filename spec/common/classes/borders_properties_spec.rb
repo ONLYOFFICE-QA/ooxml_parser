c97c7c94e03dbd4807c063e88968a709d2a78639
Pavel Lobashov <shockwavenn@gmail.com>
2019-10-24 19:03:44 +0300

    Add test for `BordersProperties`

---
+++ spec/common/classes/borders_properties_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe OoxmlParser::BordersProperties do
+  let(:default_property) { OoxmlParser::BordersProperties.new }
+  let(:init_property) { OoxmlParser::BordersProperties.new(:auto, 1) }
+
+  describe 'BordersProperties#to_s' do
+    it 'default_property to_s is empty' do
+      expect(default_property.to_s).to be_empty
+    end
+
+    it 'init_property to_s with data' do
+      expect(init_property.to_s).to include('auto')
+    end
+  end
+
+  it 'BordersProperties#copy' do
+    copy = init_property.copy
+    expect(copy).to eq(init_property)
+  end
+
+  describe 'BordersProperties#visible?' do
+    it 'default_property is not visible' do
+      expect(default_property).not_to be_visible
+    end
+
+    it 'init_property is visible' do
+      expect(init_property).to be_visible
+    end
+  end
+end
