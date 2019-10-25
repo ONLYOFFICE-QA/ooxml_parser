b3597e1573eb34562a15853643df88b0c2a716f6
Pavel Lobashov <shockwavenn@gmail.com>
2019-10-25 13:21:17 +0300

    Change argument of `ThemeColor`
    And add test for comparasion

--- lib/ooxml_parser/pptx_parser/pptx_data/presentation/presentation_theme/theme_color.rb
+++ lib/ooxml_parser/pptx_parser/pptx_data/presentation/presentation_theme/theme_color.rb
@@ -5,8 +5,9 @@ module OoxmlParser
   class ThemeColor < OOXMLDocumentObject
     attr_accessor :type, :value, :color
 
-    def initialize(type = '', parent: nil)
+    def initialize(type: '', color: nil, parent: nil)
       @type = type
+      @color = color
       @parent = parent
     end
 
--- spec/common/classes/theme_color_spec.rb
+++ spec/common/classes/theme_color_spec.rb
@@ -3,15 +3,24 @@
 require 'spec_helper'
 
 describe OoxmlParser::ThemeColor do
-  let(:system_color) { OoxmlParser::ThemeColor.new(:system) }
+  let(:system_color) do
+    OoxmlParser::ThemeColor.new(type: :system,
+                                color: OoxmlParser::Color.new(100,
+                                                              150,
+                                                              200))
+  end
   describe 'ThemeColor equality' do
     it 'ThemeColor is equal to same theme color' do
       expect(system_color).to eq(system_color)
     end
 
-    it 'ThemeColor is equal to same theme color' do
-      other_color = OoxmlParser::ThemeColor.new(:rgb)
+    it 'ThemeColor is not equal to same theme color' do
+      other_color = OoxmlParser::ThemeColor.new(type: :rgb)
       expect(system_color).not_to eq(other_color)
+    end
+
+    it 'ThemeColor is equal color of itself' do
+      expect(system_color).to eq(system_color.color)
     end
   end
 end
