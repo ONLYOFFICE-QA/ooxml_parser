465579ab70b1aee4fbaf4799400d746e2834586f
Pavel Lobashov <shockwavenn@gmail.com>
2019-10-25 12:37:29 +0300

    Add some more tests for `Workbook#cell`

--- spec/workbook/workbook_cell_spec.rb
+++ spec/workbook/workbook_cell_spec.rb
@@ -12,4 +12,12 @@ describe 'Workbook#cell' do
   it 'cell by sheet name' do
     expect(xlsx.cell(1, 4, 'Sheet2').text).to eq('Sheet2')
   end
+
+  it 'cell by nonexisting sheet name' do
+    expect(xlsx.cell(0, 0, 'Sheet3')).to be_nil
+  end
+
+  it 'sheet name not string nor integer' do
+    expect { xlsx.cell(0, 0, xlsx) }.to raise_error(RuntimeError, /Wrong sheet value/)
+  end
 end
