a8ee4d552870c29aeb5dab31ae2ddb1a71cf98d8
Pavel Lobashov <shockwavenn@gmail.com>
2019-10-25 14:55:15 +0300

    Remove `endSound` tag support. Not found any file

--- lib/ooxml_parser/pptx_parser/pptx_data/presentation/slide/transition/transition/sound_action.rb
+++ lib/ooxml_parser/pptx_parser/pptx_data/presentation/slide/transition/transition/sound_action.rb
@@ -14,8 +14,6 @@ module OoxmlParser
         case node_child.name
         when 'stSnd'
           @start_sound = Sound.new(parent: self).parse(node_child.xpath('p:snd').first) unless node_child.xpath('p:snd').first.nil?
-        when 'endSnd'
-          @end_sound = Sound.new(parent: self).parse(node_child.xpath('p:snd').first) unless node_child.xpath('p:snd').first.nil?
         end
       end
       self
