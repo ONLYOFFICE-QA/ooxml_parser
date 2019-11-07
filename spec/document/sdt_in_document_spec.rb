f410bd310a40516a26f0571698e3da8b21057ab1
Pavel Lobashov <shockwavenn@gmail.com>
2019-11-07 19:19:10 +0300

    Add test for `SDTContent#table`

--- spec/document/sdt_in_document_spec.rb
+++ spec/document/sdt_in_document_spec.rb
@@ -3,14 +3,19 @@
 require 'spec_helper'
 
 describe 'Sdt in document' do
+  let(:sdt_with_table) { OoxmlParser::Parser.parse('spec/document/sdt/sdt_with_table.docx') }
+
   it 'sdt_in_document.docx' do
     docx = OoxmlParser::DocxParser.parse_docx('spec/document/sdt/sdt_in_document.docx')
     expect(docx.elements.first.sdt_content.paragraphs.first.runs.first.text).to eq('Simple Test Text')
   end
 
   it 'sdt_with_table.docx' do
-    docx = OoxmlParser::DocxParser.parse_docx('spec/document/sdt/sdt_with_table.docx')
-    expect(docx.elements.first.sdt_content.elements.first).to be_a(OoxmlParser::Table)
+    expect(sdt_with_table.elements.first.sdt_content.elements.first).to be_a(OoxmlParser::Table)
+  end
+
+  it 'SDTContent#table' do
+    expect(sdt_with_table.elements.first.sdt_content.tables.first).to be_a(OoxmlParser::Table)
   end
 
   it 'sdt_element_parent.docx' do
