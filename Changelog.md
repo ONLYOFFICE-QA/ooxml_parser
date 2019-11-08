89040c9368b24fb7a0484405667070a1822a646b
Pavel Lobashov <shockwavenn@gmail.com>
2019-11-08 17:24:23 +0300

    Remove deprecated `GridSpan#type` and `GridSpan#count_of_merged_cells`

--- Changelog.md
+++ Changelog.md
@@ -71,6 +71,7 @@
 * Change `DocxStructure#parse_default_style` to instance method
 * Change `DocxStructure#parse_paragraph_style_xml` to instance method
 * Redone parsing `Shape`
+* Remove deprecated `GridSpan#type` and `GridSpan#count_of_merged_cells`
   
 ### Fixes
 * Fix crash on empty coordinates list of chart
---
+++ lib/ooxml_parser/common_parser/common_data/table/row/cell/grid_span.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module OoxmlParser
+  class GridSpan < OOXMLDocumentObject
+    # @return [String] type of grid span
+    attr_reader :type
+    # @return [String] value of grid span merged cells
+    attr_reader :value
+
+    def initialize(parent: nil)
+      @type = 'horizontal'
+      @parent = parent
+    end
+
+    alias count_of_merged_cells value
+
+    extend Gem::Deprecate
+    deprecate :type, 'GridSpan always horizontal', 2069, 1
+    deprecate :count_of_merged_cells, 'value', 2069, 1
+
+    # Parse Grid Span data
+    # @param [Nokogiri::XML:Element] node with GridSpan data
+    # @return [GridSpan] value of GridSpan
+    def parse(node)
+      node.attributes.each do |key, value|
+        case key
+        when 'val'
+          @value = value.value.to_i
+        end
+      end
+      self
+    end
+  end
+end
