806245248d4554699b51532e937909168b9597be
Pavel Lobashov <shockwavenn@gmail.com>
2019-11-08 18:21:38 +0300

    Add test for `SchemeColor#to_s`

---
+++ spec/common/classes/scheme_color_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe OoxmlParser::SchemeColor do
+  let(:scheme) { OoxmlParser::SchemeColor.new }
+
+  it 'OoxmlParser::SchemeColor#to_s is correct for new' do
+    expect(scheme.to_s).to eq('0')
+  end
+end
