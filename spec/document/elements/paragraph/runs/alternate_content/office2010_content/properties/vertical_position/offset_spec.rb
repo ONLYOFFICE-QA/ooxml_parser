bbb9a2dc1930febad636617246686b3a5b1ad660
Pavel Lobashov <shockwavenn@gmail.com>
2019-11-26 19:21:51 +0300

    Fix RuboCop `RSpec/HooksBeforeExamples` cop

--- spec/document/elements/paragraph/runs/alternate_content/office2010_content/properties/vertical_position/offset_spec.rb
+++ spec/document/elements/paragraph/runs/alternate_content/office2010_content/properties/vertical_position/offset_spec.rb
@@ -3,12 +3,12 @@
 require 'spec_helper'
 
 describe 'My behaviour' do
+  after do
+    OoxmlParser.reset
+  end
+
   it 'offset_in_other_unit' do
     docx = OoxmlParser::Parser.parse('spec/document/elements/paragraph/runs/alternate_content/office2010_content/properties/vertical_position/offset/offset_in_other_unit.docx')
     expect(docx.elements.first.character_style_array[2].alternate_content.office2010_content.properties.vertical_position.offset).to eq(OoxmlParser::OoxmlSize.new(914_400, :emu))
-  end
-
-  after do
-    OoxmlParser.reset
   end
 end
--- spec/document/elements/paragraph/runs/drawing/properties/shape_size/extent_spec.rb
+++ spec/document/elements/paragraph/runs/drawing/properties/shape_size/extent_spec.rb
@@ -3,6 +3,10 @@
 require 'spec_helper'
 
 describe 'My behaviour' do
+  after do
+    OoxmlParser.reset
+  end
+
   it 'extent_x_with_default_accuracy' do
     docx = OoxmlParser::DocxParser.parse_docx('spec/document/elements/paragraph/runs/drawing/properties/shape_size/extent/extent_x_with_default_accuracy.docx')
     elements = docx.element_by_description(location: :canvas, type: :paragraph)
@@ -17,9 +21,5 @@ describe 'My behaviour' do
     docx = OoxmlParser::DocxParser.parse_docx('spec/document/elements/paragraph/runs/drawing/properties/shape_size/extent/extent_x_with_custom_accuracy.docx')
     elements = docx.element_by_description(location: :canvas, type: :paragraph)
     expect(elements[0].character_style_array[0].drawing.graphic.data.properties.shape_size.extent.x).to eq(OoxmlParser::OoxmlSize.new(16.501, :centimeter))
-  end
-
-  after do
-    OoxmlParser.reset
   end
 end
