ba5252e92b2d820eddbf67de9d09ec542c761490
Pavel Lobashov <shockwavenn@gmail.com>
2020-07-09 13:13:57 +0300

    Add test to ooxml_size

--- spec/common/classes/ooxml_coordinates_spec.rb
+++ spec/common/classes/ooxml_coordinates_spec.rb
@@ -6,6 +6,6 @@ describe OoxmlParser::OOXMLCoordinates do
   let(:coord) { described_class.new(1, 2) }
 
   it '#to_s return correct value' do
-    expect(coord.to_s).to eq('(0 centimeter; 0 centimeter)')
+    expect(coord.to_s).to eq('(0.0 centimeter; 0.0 centimeter)')
   end
 end
