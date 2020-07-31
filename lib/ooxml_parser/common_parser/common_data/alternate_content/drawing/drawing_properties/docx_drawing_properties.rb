98f89086e66e399d946dd12834ab343a891ce89f
Pavel Lobashov <shockwavenn@gmail.com>
2020-07-31 10:44:26 +0300

    Move parsing `DocxDrawingProperties` inside class method

--- lib/ooxml_parser/common_parser/common_data/alternate_content/drawing/drawing_properties/docx_drawing_properties.rb
+++ lib/ooxml_parser/common_parser/common_data/alternate_content/drawing/drawing_properties/docx_drawing_properties.rb
@@ -8,9 +8,56 @@ require_relative 'docx_wrap_drawing'
 module OoxmlParser
   # Docx Drawing Properties
   class DocxDrawingProperties < OOXMLDocumentObject
-    attr_accessor :distance_from_text, :simple_position, :wrap, :object_size, :vertical_position, :horizontal_position,
-                  :relative_height
-    attr_accessor :size_relative_horizontal
-    attr_accessor :size_relative_vertical
+    # @return [DocxDrawingDistanceFromText] distance from text
+    attr_reader :distance_from_text
+    # @return [DocxWrapDrawing] wrap of drawing
+    attr_reader :wrap
+    # @return [Integer] relative height of object
+    attr_reader :relative_height
+    # @return [OOXMLCoordinates] simple position of object
+    attr_reader :simple_position
+    # @return [OOXMLCoordinates] size of object
+    attr_reader :object_size
+    # @return [DocxDrawingPosition] vertical position
+    attr_reader :vertical_position
+    # @return [DocxDrawingPosition] horizontal position
+    attr_reader :horizontal_position
+    # @return [SizeRelativeHorizontal] size of drawing relative to horizontal
+    attr_reader :size_relative_horizontal
+    # @return [SizeRelativeVertical] size of drawing relative to vertical
+    attr_reader :size_relative_vertical
+
+    # Parse DocxDrawingProperties
+    # @param [Nokogiri::XML:Node] node with DocxDrawingProperties
+    # @return [DocxDrawingProperties] result of parsing
+    def parse(node)
+      @distance_from_text = DocxDrawingDistanceFromText.new(parent: self).parse(node)
+      @wrap = DocxWrapDrawing.new(parent: self).parse(node)
+
+      node.attributes.each do |key, value|
+        case key
+        when 'relativeHeight'
+          @relative_height = value.value.to_i
+        end
+      end
+
+      node.xpath('*').each do |content_node_child|
+        case content_node_child.name
+        when 'simplePos'
+          @simple_position = OOXMLCoordinates.parse(content_node_child)
+        when 'extent'
+          @object_size = OOXMLCoordinates.parse(content_node_child, x_attr: 'cx', y_attr: 'cy', unit: :emu)
+        when 'positionV'
+          @vertical_position = DocxDrawingPosition.new(parent: self).parse(content_node_child)
+        when 'positionH'
+          @horizontal_position = DocxDrawingPosition.new(parent: self).parse(content_node_child)
+        when 'sizeRelH'
+          @size_relative_horizontal = SizeRelativeHorizontal.new(parent: self).parse(content_node_child)
+        when 'sizeRelV'
+          @size_relative_vertical = SizeRelativeVertical.new(parent: self).parse(content_node_child)
+        end
+      end
+      self
+    end
   end
 end
