b8617c98d3e11faa398ca9637796e0df7b2c12dc
Pavel Lobashov <shockwavenn@gmail.com>
2020-08-14 11:50:35 +0300

    Change `HslColor.rgb_to_hsl` to `Color#to_hsl`

--- lib/ooxml_parser/common_parser/common_data/color/color_helper.rb
+++ lib/ooxml_parser/common_parser/common_data/color/color_helper.rb
@@ -26,5 +26,34 @@ module OoxmlParser
       end
       self
     end
+
+    # Convert color to HSLColor
+    # @return [HSLColor]
+    def to_hsl
+      hls_color = HSLColor.new
+
+      min = [red, green, blue].min.to_f
+      max = [red, green, blue].max.to_f
+
+      delta = (max - min).to_f
+      hls_color.l = (min + max) / 255.0 / 2.0
+      hls_color.alpha_channel = alpha_channel.to_f / 255.0
+
+      unless max == min
+        hls_color.s = delta / (255.0 - (255.0 - max - min).abs)
+
+        hls_color.h = if max == red && green >= blue
+                        60.0 * (green - blue) / delta
+                      elsif max == red && green < blue
+                        60.0 * (green - blue) / delta + 360.0
+                      elsif max == green
+                        60.0 * (blue - red) / delta + 120.0
+                      else
+                        60.0 * (red - green) / delta + 240.0
+                      end
+      end
+
+      hls_color
+    end
   end
 end
