6866f0317d043f62aff9ba1a34dd06345f1e93b2
Pavel Lobashov <shockwavenn@gmail.com>
2020-12-09 15:53:53 +0300

    Add parsing SharedItems

--- lib/ooxml_parser/xlsx_parser/xlsx_data/view_model/workbook/pivot_cache/pivot_cache_definition/cache_fields/cache_field/shared_items.rb
+++ lib/ooxml_parser/xlsx_parser/xlsx_data/view_model/workbook/pivot_cache/pivot_cache_definition/cache_fields/cache_field/shared_items.rb
@@ -3,11 +3,38 @@
 module OoxmlParser
   # Class for parsing <sharedItems> tag
   class SharedItems < OOXMLDocumentObject
+    # @return [True, False] is contains_semi_mixed_types
+    attr_reader :contains_semi_mixed_types
+    # @return [True, False] is contains_string
+    attr_reader :contains_string
+    # @return [True, False] is contains_number
+    attr_reader :contains_number
+    # @return [True, False] is contains_integer
+    attr_reader :contains_integer
+    # @return [Integer] min value
+    attr_reader :min_value
+    # @return [Integer] max value
+    attr_reader :max_value
+
     # Parse `<sharedItems>` tag
     # # @param [Nokogiri::XML:Element] node with WorksheetSource data
     # @return [sharedItems]
     def parse(node)
       node.attributes.each do |key, value|
+        case key
+        when 'containsSemiMixedTypes'
+          @contains_semi_mixed_types = attribute_enabled?(value)
+        when 'containsString'
+          @contains_string = attribute_enabled?(value)
+        when 'containsNumber'
+          @contains_number = attribute_enabled?(value)
+        when 'containsInteger'
+          @contains_integer = attribute_enabled?(value)
+        when 'minValue'
+          @min_value = value.value.to_i
+        when 'maxValue'
+          @max_value = value.value.to_i
+        end
       end
       self
     end
