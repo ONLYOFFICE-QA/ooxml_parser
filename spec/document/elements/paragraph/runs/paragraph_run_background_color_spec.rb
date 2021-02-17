c01994156d6611539b1cd483ce47be31d095fa89
Pavel Lobashov <shockwavenn@gmail.com>
2021-02-17 18:52:06 +0300

    Add test for background color

---
+++ spec/document/elements/paragraph/runs/paragraph_run_background_color_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'ParagraphRun#background_color' do
+  let(:docx) do
+    OoxmlParser::Parser.parse('spec/document/elements/paragraph/'\
+                                        'runs/background_color/background_color.docx')
+  end
+
+  let(:background_color) { docx.elements.first.nonempty_runs.first.background_color }
+
+  it 'Background color is a specific color' do
+    expect(background_color).to eq(OoxmlParser::Color.new(226, 239, 216))
+  end
+end
