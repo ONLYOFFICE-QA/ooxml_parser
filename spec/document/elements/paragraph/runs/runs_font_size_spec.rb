ffa942b23b837ea1e90f5bc3f69e9c24eebf33d3
Pavel Lobashov <shockwavenn@gmail.com>
2021-02-18 11:27:47 +0300

    Revert changes

--- spec/document/elements/paragraph/runs/runs_font_size_spec.rb
+++ spec/document/elements/paragraph/runs/runs_font_size_spec.rb
@@ -4,7 +4,12 @@ require 'spec_helper'
 
 describe 'Runs Font Size' do
   it 'font_size_float' do
-    docx = OoxmlParser::DocxParser.parse_docx('/home/lobashov/temp/set_shd.js20210218-30830-7q608r.docx')
-    expect(docx.elements.first.background_color).to eq(OoxmlParser::Color.new(0, 255, 0))
+    docx = OoxmlParser::DocxParser.parse_docx('spec/document/elements/paragraph/runs/size/font_size_float.docx')
+    expect(docx.elements[0].character_style_array[1].size).to eq(53.5)
+  end
+
+  it 'default_font_size' do
+    docx = OoxmlParser::DocxParser.parse_docx('spec/document/elements/paragraph/runs/size/default_font_size.docx')
+    expect(docx.elements.first.character_style_array[1].size).to eq(15)
   end
 end
