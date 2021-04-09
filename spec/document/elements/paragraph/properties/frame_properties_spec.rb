979ae0136c7b265cb29b4290eabcb1ac5f84034d
Pavel Lobashov <ShockwaveNN@gmail.com>
2021-04-09 16:03:11 +0300

    Remove deprecated `DocxParagraph#frame_properties` (#756)

--- spec/document/elements/paragraph/properties/frame_properties_spec.rb
+++ spec/document/elements/paragraph/properties/frame_properties_spec.rb
@@ -7,9 +7,4 @@ describe OoxmlParser::FrameProperties do
     docx = OoxmlParser::DocxParser.parse_docx('spec/document/elements/paragraph/properties/frame_properties/anchor_lock.docx')
     expect(docx.document_styles[17].paragraph_properties.frame_properties.anchor_lock).to be_truthy
   end
-
-  it 'frame_properties_for_paragraph' do
-    docx = OoxmlParser::DocxParser.parse_docx('spec/document/elements/paragraph/properties/frame_properties/frame_properties_for_paragraph.docx')
-    expect(docx.elements[0].frame_properties.drop_cap).to eq(:drop)
-  end
 end
