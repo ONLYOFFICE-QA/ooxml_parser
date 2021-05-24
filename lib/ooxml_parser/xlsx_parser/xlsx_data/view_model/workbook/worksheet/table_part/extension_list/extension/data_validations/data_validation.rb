bbf09eb6a702dc8be9763e3ebfe1b9b4543a66aa
Pavel Lobashov <shockwavenn@gmail.com>
2021-05-24 16:40:06 +0300

    More data

--- lib/ooxml_parser/xlsx_parser/xlsx_data/view_model/workbook/worksheet/table_part/extension_list/extension/data_validations/data_validation.rb
+++ lib/ooxml_parser/xlsx_parser/xlsx_data/view_model/workbook/worksheet/table_part/extension_list/extension/data_validations/data_validation.rb
@@ -3,6 +3,12 @@
 module OoxmlParser
   # Class for `dataValidation` data
   class DataValidation < OOXMLDocumentObject
+    # @return [Boolean] should blank entries be valid
+    attr_reader :allow_blank
+    # @return [Symbol] type of error
+    attr_reader :error_style
+    # @return [Symbol] type of validation
+    attr_reader :type
     # @return [String] uid of validation
     attr_reader :uid
 
@@ -12,6 +18,12 @@ module OoxmlParser
     def parse(node)
       node.attributes.each do |key, value|
         case key
+        when 'allowBlank'
+          @allow_blank = attribute_enabled?(value)
+        when 'errorStyle'
+          @error_style = value.value.to_sym
+        when 'type'
+          @type = value.value.to_sym
         when 'uid'
           @uid = value.value.to_s
         end
--- spec/workbook/worksheet/extensions/data_validations/data_validation_spec.rb
+++ spec/workbook/worksheet/extensions/data_validations/data_validation_spec.rb
@@ -9,4 +9,16 @@ describe OoxmlParser::DataValidation do
   it 'Has uid' do
     expect(validation.uid).to eq('{0000003F-00FC-43E5-8AB4-0025003600E1}')
   end
+
+  it 'Has type' do
+    expect(validation.type).to eq(:whole)
+  end
+
+  it 'Has allow_blank' do
+    expect(validation.allow_blank).to be_truthy
+  end
+
+  it 'Has error_style' do
+    expect(validation.error_style).to eq(:stop)
+  end
 end
