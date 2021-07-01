087babecc690a9d38755697c943c0ed77b749816
alinasafonova <53475320+alinasafonova@users.noreply.github.com>
2021-07-01 18:05:14 +0300

    Add parsing header string (#790)
    * Add parsing header string
    
    * Change header parsing and add tests
    
    * Change left method
    
    * Add parsing header string
    
    * Change header parsing and add tests
    
    * Change left method
    
    * Change left method

--- lib/ooxml_parser/xlsx_parser/xlsx_data/view_model/workbook/worksheet/xlsx_header_footer/header_footer_child.rb
+++ lib/ooxml_parser/xlsx_parser/xlsx_data/view_model/workbook/worksheet/xlsx_header_footer/header_footer_child.rb
@@ -8,8 +8,9 @@ module OoxmlParser
     # @return [String] raw text of header
     attr_reader :raw_string
 
-    def initialize(type: nil, parent: nil)
+    def initialize(type: nil, raw_string: nil, parent: nil)
       @type = type
+      @raw_string = raw_string
       super(parent: parent)
     end
 
@@ -19,6 +20,37 @@ module OoxmlParser
     def parse(node)
       @raw_string = node.text
       self
+    end
+
+    # @return [String] right part of header
+    def right
+      return @right if @right
+
+      right = @raw_string.match(/&R(.+)/)
+      return nil unless right
+
+      @right = right[1]
+    end
+
+    # @return [String] center part of header
+    def center
+      return @center if @center
+
+      center = @raw_string.split('&R').first.match(/&C(.+)/)
+      return nil unless center
+
+      @center = center[1]
+    end
+
+    # @return [String] left part of header
+    def left
+      return @left if @left
+
+      left = @raw_string.gsub("&R#{right}", '')
+      left = left.gsub("&C#{center}", '')
+      return nil if left == ''
+
+      left.gsub('&L', '')
     end
   end
 end
