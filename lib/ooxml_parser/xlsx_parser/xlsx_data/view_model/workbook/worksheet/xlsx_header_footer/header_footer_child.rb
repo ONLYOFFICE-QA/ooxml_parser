932bc23ce8da8b52e48749676442a788e432b182
Alina Safonova <absolutecontrol@yandex.ru>
2021-07-01 15:29:13 +0300

    Change header parsing and add tests

--- lib/ooxml_parser/xlsx_parser/xlsx_data/view_model/workbook/worksheet/xlsx_header_footer/header_footer_child.rb
+++ lib/ooxml_parser/xlsx_parser/xlsx_data/view_model/workbook/worksheet/xlsx_header_footer/header_footer_child.rb
@@ -7,12 +7,6 @@ module OoxmlParser
     attr_reader :type
     # @return [String] raw text of header
     attr_reader :raw_string
-    # @return [String] left part of header
-    attr_reader :left
-    # @return [String] center part of header
-    attr_reader :center
-    # @return [String] right part of header
-    attr_reader :right
 
     def initialize(type: nil, raw_string: nil, parent: nil)
       @type = type
@@ -25,17 +19,40 @@ module OoxmlParser
     # @return [HeaderFooterChild] result of parsing
     def parse(node)
       @raw_string = node.text
-      parse_raw_string
       self
     end
 
-    # Perform parsing of header parts from string
-    def parse_raw_string
-      right = @raw_string.split('&R')
-      @right = right.last
-      center = right.first.split('&C')
-      @center = center.last
-      @left = center.first.split('&L').last
+    # @return [String] right part of header
+    def right
+      return @right if @right
+
+      right = @raw_string.match(/&R(.+)/)
+      return nil unless right
+
+      @right = right[1]
+    end
+
+    # @return [String] center part of header
+    def center
+      return @center if @center
+
+      center = @raw_string.split('&R').first.match(/&C(.+)/)
+      return nil unless center
+
+      @center = center[1]
+    end
+
+    # @return [String] left part of header
+    def left
+      return @left if @left
+
+      split_right = @raw_string.split('&R')
+      return nil if split_right.first == ''
+
+      left = split_right.first.split('&C').first.match(/&L(.+)/)
+      return nil unless left
+
+      @left = left[1]
     end
   end
 end
