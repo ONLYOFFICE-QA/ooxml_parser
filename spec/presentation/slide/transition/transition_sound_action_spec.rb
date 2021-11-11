9eb16fc4554b9900d48d5951a49d50409ee1204f
Pavel Lobashov <shockwavenn@gmail.com>
2021-11-11 23:27:09 +0300

    Release v0.18.1
    Fix failure if `Sound` class has no `name`

--- spec/presentation/slide/transition/transition_sound_action_spec.rb
+++ spec/presentation/slide/transition/transition_sound_action_spec.rb
@@ -2,9 +2,15 @@
 
 require 'spec_helper'
 
-describe 'My behaviour' do
+describe 'Transition#sound_action' do
   it 'tranistion_sound_action' do
     pptx = OoxmlParser::PptxParser.parse_pptx('spec/presentation/slide/transition/sound_action/tranistion_sound_action.pptx')
     expect(pptx.slides.first.transition.sound_action.start_sound.file_reference.content.length).to be > 1000
+  end
+
+  it 'sound_action_without_sound_name' do
+    pptx = OoxmlParser::PptxParser.parse_pptx('spec/presentation/slide/transition/sound_action/transition_without_sound_name.pptx')
+    expect(pptx.slides.first.transition
+               .sound_action.start_sound.name).to be_empty
   end
 end
