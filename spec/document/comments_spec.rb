3b2957f94ae9e84cb7d7e03e030945aef97bb8d0
Pavel Lobashov <shockwavenn@gmail.com>
2022-07-28 15:42:25 +0300

    Remove unused `DocxParagraph#bookmark_start/end`

--- spec/document/comments_spec.rb
+++ spec/document/comments_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 describe OoxmlParser::Comment do
   it 'comment_object.docx' do
-    docx = OoxmlParser::DocxParser.parse_docx('spec/document/comments/comment_object.docx')
+    docx = OoxmlParser::DocxParser.parse_docx('/home/lobashov/Downloads/Telegram Desktop/MT_api_numbered.docx')
     expect(docx.element_by_description(location: :comment, type: :paragraph)[0].character_style_array[0].text).to eq('Is it true?')
   end
 
