882d74510a58ae7b2d266abff4fc9503e4d15da1
Pavel Lobashov <shockwavenn@gmail.com>
2022-09-12 18:54:27 +0300

    Fix new issues

--- spec/presentation/slide/shape_grouping_spec.rb
+++ spec/presentation/slide/shape_grouping_spec.rb
@@ -5,20 +5,20 @@ require 'spec_helper'
 describe 'My behaviour' do
   it 'ShapeGrouping' do
     pptx = OoxmlParser::PptxParser.parse_pptx('spec/presentation/slide/shapes_grouping/grouping_shape.pptx')
-    expect(pptx.slides[0].elements[0]).to be_a_kind_of OoxmlParser::ShapesGrouping
+    expect(pptx.slides[0].elements[0]).to be_a(OoxmlParser::ShapesGrouping)
   end
 
   it 'grouping_chart.pptx' do
     pptx = OoxmlParser::PptxParser.parse_pptx('spec/presentation/slide/shapes_grouping/grouping_chart.pptx')
     elements = pptx.slides.first.elements
-    expect(elements.first).to be_a_kind_of OoxmlParser::ShapesGrouping
+    expect(elements.first).to be_a(OoxmlParser::ShapesGrouping)
     expect(elements[0].elements.size).to eq(2)
   end
 
   it 'grouping_with_connector.pptx' do
     pptx = OoxmlParser::PptxParser.parse_pptx('spec/presentation/slide/shapes_grouping/grouping_with_connector.pptx')
     elements = pptx.slides.first.elements
-    expect(elements.first).to be_a_kind_of OoxmlParser::ShapesGrouping
+    expect(elements.first).to be_a(OoxmlParser::ShapesGrouping)
     expect(elements[0].elements.size).to eq(2)
   end
 end
