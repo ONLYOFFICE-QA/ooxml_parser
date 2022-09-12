d69319b0414bc4c74b25e8d289a5fa61f9216c0d
Pavel Lobashov <shockwavenn@gmail.com>
2022-09-12 19:22:09 +0300

    Minor refactoring file location

--- spec/common/coordinates/coordinates_parser_coordinates_range_spec.rb
+++ spec/common/coordinates/coordinates_parser_coordinates_range_spec.rb
@@ -2,31 +2,31 @@
 
 require 'spec_helper'
 
-describe 'OoxmlParser::Coordinates' do
+describe OoxmlParser::Coordinates, '.parser_coordinates_range' do
   it 'Coordinates.parser_coordinates_range can handle single cell' do
-    coordinates = OoxmlParser::Coordinates.parser_coordinates_range('Sheet1!$B$1')
-    expect(coordinates.first).to eq(OoxmlParser::Coordinates.new(1, 'B'))
+    coordinates = described_class.parser_coordinates_range('Sheet1!$B$1')
+    expect(coordinates.first).to eq(described_class.new(1, 'B'))
   end
 
   it 'Coordinates.parser_coordinates_range can handle several ranges' do
-    coordinates = OoxmlParser::Coordinates.parser_coordinates_range('Donut!A7:A7,Donut!A16:A16')
-    expect(coordinates.first.first).to eq(OoxmlParser::Coordinates.new(7, 'A'))
+    coordinates = described_class.parser_coordinates_range('Donut!A7:A7,Donut!A16:A16')
+    expect(coordinates.first.first).to eq(described_class.new(7, 'A'))
   end
 
   it 'Coordinates.parser_coordinates_range cannot handle complex formula' do
     expect do
-      OoxmlParser::Coordinates.parser_coordinates_range("'file!'file:xls'#'Ecc-C2345'.AJ23:'file:xls'#'Ecc-C2345'.AJ236")
+      described_class.parser_coordinates_range("'file!'file:xls'#'Ecc-C2345'.AJ23:'file:xls'#'Ecc-C2345'.AJ236")
     end.to output(/Formulas with # is unsupported/).to_stderr
   end
 
   it 'Coordinates.parser_coordinates_range not crash if formula just !' do
     expect do
-      OoxmlParser::Coordinates.parser_coordinates_range('!')
+      described_class.parser_coordinates_range('!')
     end.to output(/Formulas consists from `!` only/).to_stderr
   end
 
   it 'Coordinates.parser_coordinates_range not crash in formulas with alot of !' do
-    coordinates = OoxmlParser::Coordinates.parser_coordinates_range('!!!!Сроки!$A$1:$E$1')
+    coordinates = described_class.parser_coordinates_range('!!!!Сроки!$A$1:$E$1')
     expect(coordinates.length).to eq(5)
   end
 end
