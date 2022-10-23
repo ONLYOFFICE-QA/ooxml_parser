96149004f7bd7e5403f082d2fe32af5e2c10887c
Pavel Lobashov <shockwavenn@gmail.com>
2022-10-23 23:02:42 +0300

    Fix more

--- lib/ooxml_parser/common_parser/common_data/colors/color_alpha_channel.rb
+++ lib/ooxml_parser/common_parser/common_data/colors/color_alpha_channel.rb
@@ -11,7 +11,12 @@ module OoxmlParser
       alpha_channel_node = node.xpath('w14:alpha', 'xmlns:w14' => 'http://schemas.microsoft.com/office/word/2010/wordml').first if alpha_channel_node.nil?
       return 100.0 if alpha_channel_node.nil?
 
-      (alpha_channel_node.attribute('val').value.to_f / 1_000.0).round(0)
+      node.attributes.each do |key, value|
+        case key
+        when 'val'
+          return (value.value.to_f / 1_000.0).round(0)
+        end
+      end
     end
   end
 end
