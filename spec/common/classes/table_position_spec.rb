e5aa988a154d7cbd3dd0ade70ee04bb5e8dc7e4b
Pavel Lobashov <shockwavenn@gmail.com>
2022-10-27 12:37:24 +0300

    Increase test coverage

--- spec/common/classes/table_position_spec.rb
+++ spec/common/classes/table_position_spec.rb
@@ -9,5 +9,16 @@ describe OoxmlParser::TablePosition do
     it 'table_position.to_s is outputted for empty class' do
       expect(table_position.to_s).to include('Table position')
     end
+
+    it 'table_position contains all arguments in .to_s' do
+      table_position = described_class.new
+      table_position.left = 1
+      table_position.right = 2
+      table_position.top = 3
+      table_position.bottom = 4
+      table_position.position_x = 5
+      table_position.position_y = 6
+      expect(table_position.to_s).not_to be_empty
+    end
   end
 end
