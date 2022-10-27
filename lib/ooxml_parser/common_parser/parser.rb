64e18e2450dd2dced784d8b9e5297f9bdffda40c
Pavel Lobashov <shockwavenn@gmail.com>
2022-10-27 15:49:26 +0300

    Add logs

--- lib/ooxml_parser/common_parser/parser.rb
+++ lib/ooxml_parser/common_parser/parser.rb
@@ -11,6 +11,7 @@ module OoxmlParser
       # @param [OoxmlFile] file with data
       # @return [CommonDocumentStructure] structure of doc
       def parse_format(file)
+        puts "Start parsing #{file}"
         return nil if EncryptionChecker.new(file.path).encrypted?
 
         file.copy_file_and_rename_to_zip
