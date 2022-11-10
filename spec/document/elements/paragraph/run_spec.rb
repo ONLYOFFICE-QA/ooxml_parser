0a2704c71e508badd5ad4cbb2c74792cdcf085f1
Pavel Lobashov <ShockwaveNN@gmail.com>
2022-11-10 11:47:39 +0200

    Fix early exit from parsing default run style (#1057)
    * Fix early exit from parsing default run style
    
    Without this fix if default style has no paragraph properties,
    but have run properties so looks like that:
    ```
    <w:style w:type="paragraph" w:styleId="598" w:default="1">
    <w:name w:val="Normal"/>
    <w:qFormat/>
    <w:rPr>
    <w:rFonts w:hint="default" w:ascii="Calibri Light" w:hAnsi="Calibri Light" w:eastAsia="Calibri Light" w:cs="Calibri Light"/>
    <w:color w:val="262626"/>
    <w:sz w:val="32"/>
    </w:rPr>
    </w:style>
    ```
    
    RunProperties just ignored because method
    `parse_default_paragraph_style` just exited from it if there no `pPr`
    tag
    
    * Fix more

--- spec/document/elements/paragraph/run_spec.rb
+++ spec/document/elements/paragraph/run_spec.rb
@@ -7,4 +7,17 @@ describe OoxmlParser::ParagraphRun do
     docx = OoxmlParser::DocxParser.parse_docx('spec/document/elements/paragraph/runs/caps_characters_on.docx')
     expect(docx.elements.first.character_style_array.first.caps).to eq(:caps)
   end
+
+  it 'font properties' do
+    docx = OoxmlParser::DocxParser.parse_docx('spec/document/elements/paragraph/runs/run_font_properties.docx')
+    expect(docx.elements.first.nonempty_runs[0].text).to eq('This is a paragraph with the text color, font family and font size set using the text style. ')
+    expect(docx.elements.first.nonempty_runs[0].font).to eq('Calibri Light')
+    expect(docx.elements.first.nonempty_runs[0].font_color).to eq(OoxmlParser::Color.new(38, 38, 38))
+    expect(docx.elements.first.nonempty_runs[1].text).to eq('We do not change the style of the paragraph itself. ')
+    expect(docx.elements.first.nonempty_runs[1].font_color).to eq(OoxmlParser::Color.new(38, 38, 38))
+    expect(docx.elements.first.nonempty_runs[1].font).to eq('Calibri Light')
+    expect(docx.elements.first.nonempty_runs[2].text).to eq('Only document-wide text styles are applied.')
+    expect(docx.elements.first.nonempty_runs[2].font_color).to eq(OoxmlParser::Color.new(38, 38, 38))
+    expect(docx.elements.first.nonempty_runs[2].font).to eq('Calibri Light')
+  end
 end
