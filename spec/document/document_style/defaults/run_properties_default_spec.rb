afd3cd1f5bf196f8876503723783d8114f3c7bb3
Pavel Lobashov <shockwavenn@gmail.com>
2022-11-10 08:52:41 +0200

    Do not crash if default paragraph style have no run properties

--- spec/document/document_style/defaults/run_properties_default_spec.rb
+++ spec/document/document_style/defaults/run_properties_default_spec.rb
@@ -3,6 +3,11 @@
 require 'spec_helper'
 
 describe 'My behaviour' do
+  it 'parser will not crash if there is no default run style' do
+    docx = OoxmlParser::DocxParser.parse_docx('spec/document/document_style/defaults/run_properties/no_default_run_properties.docx')
+    expect(docx).to be_with_data
+  end
+
   it 'default_document_language' do
     docx = OoxmlParser::DocxParser.parse_docx('spec/document/document_style/defaults/run_properties/default_document_language.docx')
     expect(docx.styles.document_defaults.run_properties_default.run_properties.language.value).to eq('en-CA')
