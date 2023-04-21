77d23864bc4bbcc145b171c4004dc6d5e0de7ea6
OnlyOffice Testing Robot <teamlab.ruby@gmail.com>
2023-04-21 17:33:34 +0300

    Remove unused parameter from spec (#1136)
    Also simplify spec a little bit

--- spec/ooxml_parser/paragraph_borders_spec.rb
+++ spec/ooxml_parser/paragraph_borders_spec.rb
@@ -4,7 +4,11 @@ require 'spec_helper'
 
 describe OoxmlParser::ParagraphBorders do
   let(:empty_border) { described_class.new }
-  let(:init_property) { described_class.new(:auto, 1) }
+  let(:single_line_border) do
+    OoxmlParser::BordersProperties.new(:auto,
+                                       1,
+                                       :single)
+  end
 
   describe '#border_visual_type' do
     before do
@@ -20,44 +24,44 @@ describe OoxmlParser::ParagraphBorders do
     end
 
     it 'left border_visual_type' do
-      empty_border.left = OoxmlParser::BordersProperties.new(:auto, 1, :single)
+      empty_border.left = single_line_border
       expect(empty_border.border_visual_type).to eq(:left)
     end
 
     it 'right border_visual_type' do
-      empty_border.right = OoxmlParser::BordersProperties.new(:auto, 1, :single)
+      empty_border.right = single_line_border
       expect(empty_border.border_visual_type).to eq(:right)
     end
 
     it 'top border_visual_type' do
-      empty_border.top = OoxmlParser::BordersProperties.new(:auto, 1, :single)
+      empty_border.top = single_line_border
       expect(empty_border.border_visual_type).to eq(:top)
     end
 
     it 'bottom border_visual_type' do
-      empty_border.bottom = OoxmlParser::BordersProperties.new(:auto, 1, :single)
+      empty_border.bottom = single_line_border
       expect(empty_border.border_visual_type).to eq(:bottom)
     end
 
     it 'inner border_visual_type' do
-      empty_border.between = OoxmlParser::BordersProperties.new(:auto, 1, :single)
+      empty_border.between = single_line_border
       expect(empty_border.border_visual_type).to eq(:inner)
     end
 
     it 'all border_visual_type' do
-      empty_border.top = OoxmlParser::BordersProperties.new(:auto, 1, :single)
-      empty_border.bottom = OoxmlParser::BordersProperties.new(:auto, 1, :single)
-      empty_border.left = OoxmlParser::BordersProperties.new(:auto, 1, :single)
-      empty_border.right = OoxmlParser::BordersProperties.new(:auto, 1, :single)
-      empty_border.between = OoxmlParser::BordersProperties.new(:auto, 1, :single)
+      empty_border.top = single_line_border
+      empty_border.bottom = single_line_border
+      empty_border.left = single_line_border
+      empty_border.right = single_line_border
+      empty_border.between = single_line_border
       expect(empty_border.border_visual_type).to eq(:all)
     end
 
     it 'outer border_visual_type' do
-      empty_border.top = OoxmlParser::BordersProperties.new(:auto, 1, :single)
-      empty_border.bottom = OoxmlParser::BordersProperties.new(:auto, 1, :single)
-      empty_border.left = OoxmlParser::BordersProperties.new(:auto, 1, :single)
-      empty_border.right = OoxmlParser::BordersProperties.new(:auto, 1, :single)
+      empty_border.top = single_line_border
+      empty_border.bottom = single_line_border
+      empty_border.left = single_line_border
+      empty_border.right = single_line_border
       expect(empty_border.border_visual_type).to eq(:outer)
     end
   end
