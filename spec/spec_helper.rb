39e27a12bc9d2f759502b4f1194b0c3991d44cdf
ONLYOFFICE Testing Robot <teamlab.ruby@gmail.com>
2023-04-21 12:11:19 +0300

    Skip coverage for truffle ruby

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
-require 'simplecov'
-SimpleCov.start do
-  add_filter('/spec/')
+# Until https://github.com/oracle/truffleruby/issues/2813 is released
+unless RUBY_ENGINE == 'truffleruby'
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter('/spec/')
+  end
 end
 
 require 'ooxml_parser'
