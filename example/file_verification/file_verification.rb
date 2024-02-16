c1b107109497cb45024989a19a032e303c1cc4b3
Vitaliy Serov <vitaliy.serov@onlyoffice.com>
2024-02-16 14:06:45 +0200

    Fix more

--- example/file_verification/file_verification.rb
+++ example/file_verification/file_verification.rb
@@ -3,35 +3,46 @@
 require 'optparse'
 require 'ooxml_parser'
 
+# Parse command line option
+def parse_command_line
+  options = {}
+
+  OptionParser.new do |opts|
+    opts.on('--file FILE', 'Specify the DOCX file') do |file|
+      options[:file] = file
+    end
+  end.parse!
+
+  if options[:file].nil?
+    puts 'Error: Please provide a DOCX file using --file parameter.'
+    exit(255)
+  end
+  options
+end
+
+# Verify the file
 def verification(file_path)
   docx = OoxmlParser::DocxParser.parse_docx(file_path)
   docx.background.color1 == OoxmlParser::Color.new(255, 255, 255)
 end
 
-options = {}
-
-OptionParser.new do |opts|
-  opts.on('--file FILE', 'Specify the DOCX file') do |file|
-    options[:file] = file
+# Verify the file and report the results
+def verify_and_report(file)
+  begin
+    result = verification(file)
+  rescue StandardError => e
+    puts "Error: #{e.message}"
+    result = false
   end
-end.parse!
 
-if options[:file].nil?
-  puts 'Error: Please provide a DOCX file using --file parameter.'
-  exit(255)
+  if result
+    puts 'File is correct'
+    exit(0)
+  else
+    puts 'Something is wrong with the file'
+    exit(1)
+  end
 end
 
-begin
-  result = verification(options[:file])
-rescue StandardError => e
-  puts "Error: #{e.message}"
-  result = false
-end
-
-if result
-  puts 'File is correct'
-  exit(0)
-else
-  puts 'Something is wrong with the file'
-  exit(1)
-end
+options = parse_command_line
+verify_and_report(options[:file])
