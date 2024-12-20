935aab84f9a0e8180135eb1d297929fa34a3b563
Vitaliy Serov <vitaliy.serov@onlyoffice.com>
2024-12-20 13:12:32 +0200

    Monkeypatch `File::SHARE_DELETE` to be compatible with `truffleruby-24.1`

--- lib/ooxml_parser.rb
+++ lib/ooxml_parser.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+# Monkey patch around
+# https://github.com/oracle/truffleruby/commit/cc76155bf509587e1b2954e9de77c558c7c857f4
+# Remove it as soon as resolved
+if defined?(Truffle)
+  class File < IO
+    SHARE_DELETE   = 0
+  end
+end
+
 require_relative 'ooxml_parser/common_parser'
 require_relative 'ooxml_parser/configuration'
 require_relative 'ooxml_parser/helpers/string_helper'
