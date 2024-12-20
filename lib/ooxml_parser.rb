214f2ecbdfdb3f964e9494f3a24c309d0b183eb5
Vitaliy Serov <vitaliy.serov@onlyoffice.com>
2024-12-20 13:15:11 +0200

    Fix comment

--- lib/ooxml_parser.rb
+++ lib/ooxml_parser.rb
@@ -2,7 +2,7 @@
 
 # Monkey patch around
 # https://github.com/oracle/truffleruby/commit/cc76155bf509587e1b2954e9de77c558c7c857f4
-# Remove it as soon as resolved
+# Remove it as soon as TruffleRuby with fix is released
 if defined?(Truffle)
   # MonkeyPatch File stdlib class
   class File < IO
