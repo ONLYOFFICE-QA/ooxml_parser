b6098aeaea24938483c3e03fd69993081d6095cf
Vitaliy Serov <vitaliy.serov@onlyoffice.com>
2024-12-20 13:14:21 +0200

    Add missing comments

--- lib/ooxml_parser.rb
+++ lib/ooxml_parser.rb
@@ -4,7 +4,9 @@
 # https://github.com/oracle/truffleruby/commit/cc76155bf509587e1b2954e9de77c558c7c857f4
 # Remove it as soon as resolved
 if defined?(Truffle)
+  # MonkeyPatch File stdlib class
   class File < IO
+    # Monkey patch constant missing on TruffleRuby
     SHARE_DELETE   = 0
   end
 end
