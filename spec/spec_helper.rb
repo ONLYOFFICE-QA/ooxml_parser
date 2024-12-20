702c15c1fcd4af0b73323f9cfddb0f596b357d29
Vitaliy Serov <vitaliy.serov@onlyoffice.com>
2024-12-20 13:31:26 +0200

    Fix coverage calculations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,10 @@
 require 'simplecov'
 SimpleCov.start do
   add_filter('/spec/')
+  # Remove as soon as
+  # https://github.com/oracle/truffleruby/commit/cc76155bf509587e1b2954e9de77c558c7c857f4
+  # is released
+  add_filter('/lib/ooxml_parser.rb')
 end
 
 require 'ooxml_parser'
