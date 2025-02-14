191234d592205e6e69d5a96dd0a9eaacbd08bebf
Vitaliy Serov <154601125+VitaliySerov@users.noreply.github.com>
2025-02-14 15:44:52 +0200

    Add parsing `Comment` parameters `author`, `date`, `initials` (#1457)

--- spec/document/comments_spec.rb
+++ spec/document/comments_spec.rb
@@ -3,9 +3,24 @@
 require 'spec_helper'
 
 describe OoxmlParser::Comment do
+  let(:docx) { OoxmlParser::DocxParser.parse_docx('spec/document/comments/comment_object.docx') }
+
   it 'comment_object.docx' do
-    docx = OoxmlParser::DocxParser.parse_docx('spec/document/comments/comment_object.docx')
     expect(docx.element_by_description(location: :comment, type: :paragraph)[0].character_style_array[0].text).to eq('Is it true?')
+  end
+
+  describe 'Comment additional params' do
+    it 'comment object have auth' do
+      expect(docx.comments[0].author).to eq('Hamish Mitchell')
+    end
+
+    it 'comment object have date in string format' do
+      expect(docx.comments[0].date_string).to eq('2015-04-06T13:09:31Z')
+    end
+
+    it 'comment object have initials' do
+      expect(docx.comments[0].initials).to eq('HM')
+    end
   end
 
   it 'comments_paragraphs_with_ids.docx' do
